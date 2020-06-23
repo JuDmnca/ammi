46c0843595ba3777627e355af917468990c3bc3f
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2020-06-23 17:41:15 +0200

    Update index.php
    ctrl+c from webhost

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-
 <?php
-session_start();
 include_once("php/code.php");
 
 $user = new Users;
 $work = new Works;
 require('php/session.php');
-?>
 
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -23,22 +21,23 @@ require('php/session.php');
       <link rel="stylesheet" type="text/css" href="css/style.css">
 
   </head>
-  <body class="whitetext" data-barba="wrapper">
+  <body class='index_body whitetext' data-barba="wrapper">
     <div class="loading-container">
       <div class="loading-screen">
       </div>
     </div>
-    <div class="vimeo-wrapper">
+    <main data-barba="container" data-barba-namespace="home">
+        <p class='credits'>Background by <a href='https://www.instagram.com/yoshisodeoka/?hl=en'>Yoshi Sodeoka</a></p>
+        <div class="vimeo-wrapper">
       <div class="overlay"></div>
       <iframe src="https://player.vimeo.com/video/412813271?background=1&autoplay=1&mute=1&loop=1"
       frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
     </div>
-    <main data-barba="container" data-barba-namespace="home">
       <nav class="navbar navbar-light project">
-          <a class="navbar-brand bold" href="index.php">A-MMI</a>
-          <div class="ml-auto">
-            <a class="navbar-brand bold" href="/collection.php">Our collection</a>
-            <a class="navbar-brand bold" href="/about.php">About us</a>
+          <a class="navbar-brand bold " href="index.php">A-MMI</a>
+          <div class="ml-auto ">
+            <a class="navbar-brand whitetext bold" href="collection.php">Our collection</a>
+            <a class="navbar-brand whitetext bold" href="about.php">About us</a>
             <?php
                 if($Connected == true){
             ?>
@@ -47,7 +46,7 @@ require('php/session.php');
               echo($_SESSION["account"]["username"]);
               }
               ?></p>
-              <a class="navbar-brand d-inline whitetext" href="logout.php">LOGOUT</a>
+              <a class="navbar-brand d-inline" href="logout.php">LOGOUT</a>
           </div>
           <?php }
           ?>
@@ -56,20 +55,24 @@ require('php/session.php');
 
       <div class="container main">
 
-        <h1 class="is-animated whitetext">We are <span class="bold">A-MMI</span>,</h1>
-        <h1 class="is-animated whitetext">born from creativity.</h1>
+        <h1 class="is-animated">We are <span class="bold">A-MMI</span>,</h1>
+        <h1 class="is-animated">born from creativity.</h1>
 
         <ul class="list-group list-group-horizontal main fadein">
           <li class="list-group-item bold whitetext">Our latest projects</li>
           <?php
             $allworks = $work->get_works();
             foreach($allworks as $w)
-          { ?>
+            {
+          ?>
           <li class="list-group-item whitetext"><a href="project.php?id=<?= $w["id"] ?>"><?php echo($w["title"]); ?></a></li>
-          <?php } ?>
+          <?php
+            }
+          ?>
         </ul>
       </div>
     </main>
+
     <!-- JQuery -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
@@ -84,9 +87,6 @@ require('php/session.php');
 
     <!-- Main JS file -->
     <script src="js/main.js"></script>
-
-    <!-- Remove Banner -->
-   <script src="js/removebanner.js"></script>
 
   </body>
 </html>
