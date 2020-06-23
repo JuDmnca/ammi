f8403f15ee1154cec1bfcc5b69e2b14d568e90f3
Julie De Mendonça <43884258+JuDmnca@users.noreply.github.com>
2020-06-23 15:44:37 +0200

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,16 +1,24 @@
 <?php
 
-	$DB_DSN = "pgsql:host=dc0i5j4frkacqj;host=ec2-46-137-124-19.eu-west-1.compute.amazonaws.com
+$host = "ec2-46-137-124-19.eu-west-1.compute.amazonaws.com
 ";
-	$DB_USER = "ezsixynnhzciyd";
-	$DB_PASSWORD = "469ba3bf25bcbe7d3947b77a1fac035b11a161eb880e6da99fba55919b8a887a";
+$user = "ezsixynnhzciyd";
+$password = "469ba3bf25bcbe7d3947b77a1fac035b11a161eb880e6da99fba55919b8a887a";
+$dbname = "dc0i5j4frkacqj";
+$port = "5432";
 
-	try {
-		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
-		$db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch (PDOException $e) {
-		echo 'Echec de la connexion : ' . $e->getMessage();
-	}
+try{
+  //Set DSN data source name
+    $dsn = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
 
-?>
+
+  //create a pdo instance
+  $pdo = new PDO($dsn, $user, $password);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+  $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+echo 'Echec de la connexion: ' . $e->getMessage();
+}
+  ?>
