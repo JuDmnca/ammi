46426692f3369a7a2c0210e080ef799256b14aa9
Julie De Mendonça <43884258+JuDmnca@users.noreply.github.com>
2020-06-23 15:23:04 +0200

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,8 +1,9 @@
 <?php
 
-	$DB_DSN = "mysql:dbname=php;host=localhost";
-	$DB_USER = "php";
-	$DB_PASSWORD = "phpmmi";
+	$DB_DSN = "mysql:dbname=dc0i5j4frkacqj;host=ec2-46-137-124-19.eu-west-1.compute.amazonaws.com
+";
+	$DB_USER = "ezsixynnhzciyd";
+	$DB_PASSWORD = "469ba3bf25bcbe7d3947b77a1fac035b11a161eb880e6da99fba55919b8a887a";
 
 	try {
 		$db = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
