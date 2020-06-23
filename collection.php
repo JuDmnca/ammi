57ae0cff72550eb1c50fa2c974109573ee5c6ae9
Julie De Mendonça <julie.demendonca@mmibordeaux.com>
2020-06-23 18:12:34 +0200

    Update collection.php

--- collection.php
+++ collection.php
@@ -35,7 +35,7 @@ require ('php/session.php');
             <a class="navbar-brand bold" href="about.php">About us</a>
             <?php if($Connected == true){?>
             <p class="navbar-brand d-inline"><?php if(isset($_SESSION["account"]["username"]))
-            {echo($_SESSION["account"]["username"]);}?>
+            {echo($_SESSION["account"]["username"]);};?>
             </p>
             <a class="navbar-brand d-inline" href="logout.php">LOGOUT</a>
             <?php}?>
