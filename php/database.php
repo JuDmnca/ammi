2423be2ad401665874fe576e75b1d8c04bed4982
Julie De Mendonça <43884258+JuDmnca@users.noreply.github.com>
2020-06-23 15:37:34 +0200

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,6 +1,6 @@
 <?php
 
-	$DB_DSN = "mysql:dbname=dc0i5j4frkacqj;host=ec2-46-137-124-19.eu-west-1.compute.amazonaws.com
+	$DB_DSN = "pgsql:host=dc0i5j4frkacqj;host=ec2-46-137-124-19.eu-west-1.compute.amazonaws.com
 ";
 	$DB_USER = "ezsixynnhzciyd";
 	$DB_PASSWORD = "469ba3bf25bcbe7d3947b77a1fac035b11a161eb880e6da99fba55919b8a887a";
